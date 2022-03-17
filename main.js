d1995a1d674e97a3b9120d4d2ecbc96e88642a5e
Juan Manuel Rivera <jm.rivera@uniandes.edu.co>
2022-03-16 20:36:19 -0500

    Carga fichas con comida

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let array = [];
 
 let gruposProductos = [];
 
+let grupoActual = 0;
+
 class Producto {
   constructor(name, description, price, image) {
     this.name = name;
@@ -28,6 +30,71 @@ function getData(callback) {
       console.log("Resultados", res);
       callback(res);
     });
+}
+
+function filtrarProductos(categoria) {}
+
+function mostrarTarjetas(idGrupo) {
+  let grupo = gruposProductos[idGrupo];
+  vitrina = document.getElementById("vitrina");
+  tituloGrupo = document.getElementById("tituloGrupo");
+  tituloGrupo.innerHTML = grupo.name;
+
+  let row = document.createElement("div");
+  row.className = "row";
+
+  for (let i = 0; i < Object.keys(grupo).length; i++) {
+    let producto = grupo.productos[i];
+    let division = document.createElement("div");
+    let imagen = document.createElement("img");
+
+    division.className = "col-6 col-md-3 mt-4 card mx-auto";
+    division.style = "width: 18rem";
+
+    imagen.className = "card-img-top";
+    imagen.src = producto.image;
+
+    let cuerpo = document.createElement("div");
+    cuerpo.className = "card-body";
+
+    let titulo = document.createElement("h5");
+    titulo.className = "card-title text-center titulos";
+    titulo.innerHTML = producto.name;
+
+    let descripcion = document.createElement("p");
+    descripcion.className = "textoPlano";
+    descripcion.innerHTML = producto.description;
+
+    let precio = document.createElement("p");
+    precio.className = "precio";
+    precio.innerHTML = "$ " + producto.price;
+
+    let button = document.createElement("a");
+    button.className = "btn";
+    button.innerHTML = "Comprar";
+
+    cuerpo.appendChild(titulo);
+    cuerpo.appendChild(descripcion);
+    cuerpo.appendChild(precio);
+    cuerpo.appendChild(button);
+
+    division.appendChild(imagen);
+    division.appendChild(cuerpo);
+
+    row.appendChild(division);
+    vitrina.appendChild(row);
+  }
+}
+
+function llenarMenu() {
+  menu = document.getElementById("Menu");
+
+  for (let i = 0; i < gruposProductos.length; i++) {
+    let grupo = document.createElement("li");
+    grupo.className = "menuItem";
+    grupo.innerHTML = gruposProductos[i].name;
+    menu.appendChild(grupo);
+  }
 }
 
 getData((value) => {
@@ -57,6 +124,9 @@ getData((value) => {
     gruposProductos.push(grupo);
     console.log("Grupo ya creado", grupo);
   }
+
+  llenarMenu();
+  mostrarTarjetas(0);
 });
 
 //console.log("Elemento", document.getElementsByTagName("p"));*/
